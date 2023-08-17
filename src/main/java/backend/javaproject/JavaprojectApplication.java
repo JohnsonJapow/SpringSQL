b10d09204c8af91028c38940e70d7c1125bb33c2
JohnsonJapow <x21174237@student.ncirl.ie>
2023-08-17 15:13:36 +0100

    your message

--- src/main/java/backend/javaproject/JavaprojectApplication.java
+++ src/main/java/backend/javaproject/JavaprojectApplication.java
@@ -2,10 +2,15 @@ package backend.javaproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JavaprojectApplication {
 
+	@GetMapping("/message")
+	public String message(){
+		return "Congrats, your app is deployed completely";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(JavaprojectApplication.class, args);
 	}
